cb8dc7f3faeaa681b76489a0694add4f2b012373
刘老六 <rrsyycm@163.com>
2023-03-29 00:33:35 +0800

    图标替换

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
 </template>
 <script lang="ts" setup>
-console.log('env', process.env.NODE_ENV)
+import {onBeforeMount} from "vue"
+// #ifdef APP-NVUE || APP-PLUS-NVUE
+//customfont为你的base64的ttf字体内容
+import {customfont} from "./static/icon/customiconfont";
+
+var domModule = weex.requireModule("dom");
+const animation = uni.requireNativePlugin("animation");
+// #endif
+
+// #ifdef APP-PLUS-NVUE
+console.log('APP-PLUS-NVUE')
+onBeforeMount(() => {
+  //customfont为你的base64的ttf字体内容
+  domModule.addRule("fontFace", {
+    fontFamily: "td", //注意这里必须是驼峰命名，跟上面的css样式对照
+    src: "url('data:font/ttf;charset=utf-8;base64," + customfont + "')",
+  });
+});
+// #endif
 </script>
 <style>
-@import './static/icon/iconfont-weapp-icon.css';
+
 /* #ifdef APP-PLUS-NVUE */
 @import './tmui/scss/nvue.css';
 
 /* #endif */
 
 /* #ifndef APP-PLUS-NVUE */
-
+@import '@/static/icon/custom.css';
 @import './tmui/scss/noNvue.css';
 
 /* #endif */
